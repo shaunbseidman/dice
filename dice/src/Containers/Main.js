deeb822e81c6c8543cd011e02a42503db395d337
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-13 15:00:51 -0600

    removed extra components

---
+++ dice/src/Containers/Main.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import Button from '../Components/Button'
+import './Main.css';
+
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      min: 1,
+      max: 6,
+      number: this.generateNumber(1, 6)
+    }
+  }
+
+  generateNumber = (min, max) => {
+    const newNumber = Math.floor(Math.random() * (max - min + 1) + min)
+    this.setState({number: newNumber})
+    return newNumber
+  }
+
+  getNumber = () => {
+    let minTemp = this.state.min
+    let maxTemp = this.state.max
+    if (minTemp > maxTemp) {
+      this.setState({
+        min: maxTemp,
+        max: minTemp
+      }, () => this.generateNumber(this.state.min, this.state.max))
+    } else {
+      this.generateNumber(minTemp, maxTemp)
+    }
+  }
+
+  render() {
+    return (<div>
+      <h1>Roll the Dice</h1>
+      <p>{this.state.number}</p>
+      <button onClick={this.getNumber}>Roll the Dice</button>
+    </div>)
+  }
+}
+
+export default Main
